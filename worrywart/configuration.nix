8c8d7e81b3bc637724e9a0e5c825746e1a37056a
James Williams <51956016+jamesreprise@users.noreply.github.com>
2023-02-09 22:22:39 +0000

    feat: worrywart for portfolio

---
+++ worrywart/configuration.nix
@@ -0,0 +1,39 @@
+{ config, pkgs, ... }:
+
+{
+  imports = [ stagit.nix ];
+
+  # Services
+  services.openssh.permitRootLogin = "prohibit-password";
+
+  # Users
+  users.mutableUsers = false;
+  users.users.james = {
+    isNormalUser = true;
+    home = "/home/james";
+    group = "james";
+    hashedPassword = "...";
+    extraGroups = ["wheel"];
+  };
+  users.groups.james = {};
+  services.openssh.enable = true;
+
+
+  # Nix 
+  nix.nixPath = [
+    "nixpkgs=https://nixos.org/channels/nixpkgs-unstable"
+    "nixos-config=/etc/nixos/configuration.nix"
+  ];
+
+  # This value determines the NixOS release with which your system is to be
+  # compatible, in order to avoid breaking some software such as database
+  # servers. You should change this only after NixOS release notes say you
+  # should.
+  system.stateVersion = "22.11"; 
+  
+
+  # System packages 
+  environment.systemPackages = with pkgs; [ 
+    ncdu htop tmux git (callPackage <agenix/pkgs/agenix.nix> {}) 
+  ];
+}
