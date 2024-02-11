bd3e2ffdd7094b267e0661e96a0e66e662fcc95e
James Williams <james@berserksystems.com>
2024-02-11 16:22:41 +0000

    feat(new-island): inline zshrc

--- new-island/home-manager/terminal.nix
+++ new-island/home-manager/terminal.nix
@@ -2,10 +2,26 @@
 {
   programs.zsh = {
     enable = true;
-    initExtraFirst = builtins.readFile ./zshrc;
     initExtra = ''
-    export OPENSSL_DIR="${pkgs.openssl.dev}"
-    export OPENSSL_LIB_DIR="${pkgs.openssl.out}/lib"
+      # PATH and envvars
+      export ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE="fg=#d78700"
+      export GPG_TTY=$(tty)
+      export REPO_VATICLE_USERNAME=...
+      export REPO_VATICLE_PASSWORD=...
+      export ARTIFACT_USERNAME=$REPO_VATICLE_USERNAME
+      export ARTIFACT_PASSWORD=$REPO_VATICLE_PASSWORD
+      export DEPLOY_HELM_USERNAME=$REPO_VATICLE_USERNAME
+      export DEPLOY_HELM_PASSWORD=$REPO_VATICLE_PASSWORD
+      # Gcloud
+      export USE_GKE_GCLOUD_AUTH_PLUGIN=True
+      # Aliases
+      alias vim=nvim
+      alias cd=z
+      # Don't like it? Come kill me. 
+      alias nano=nvim
+      alias k=kubectl
+      alias b=bazel
+      [[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
     '';
     prezto = {
       enable = true;
