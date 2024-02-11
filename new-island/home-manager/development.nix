2a42ae577f9e128f6034b459dd0a68064e693091
James Williams <james@berserksystems.com>
2024-02-11 16:26:21 +0000

    feat(new-island): go support, airline, toggleterm, etc

--- new-island/home-manager/development.nix
+++ new-island/home-manager/development.nix
@@ -47,6 +47,8 @@ in
     (texlive.combine {
       inherit (texlive) scheme-full kpfonts fontspec titlesec enumitem changepage;
     })
+    # Go
+    go gopls
     # Misc  
     file mpv imagemagick
   ] ++ scripts;
@@ -72,38 +74,62 @@ in
     vimdiffAlias = true;
     defaultEditor = true;
     extraConfig = ''
-        set ignorecase
-        set background=dark
-        set number
-        set mouse=a
-        set tabstop=4
-        set shiftwidth=4
-        set softtabstop=4
-        set expandtab
-        set hidden
-        set t_Co=256
-        nnoremap <Space> i
-        inoremap <S-Space> <Esc>
+      set ignorecase
+      set background=dark
+      set number
+      set mouse=a
+      set tabstop=4
+      set shiftwidth=4
+      set softtabstop=4
+      set expandtab
+      set hidden
+      set t_Co=256
+      nnoremap <Space> i
+      inoremap <S-Space> <Esc>
 
-        let mapleader = ","
-        let maplocalleader = ","
+      set omnifunc=syntaxcomplete#Complete
 
-        colorscheme gruvbox
-        highlight NormalFloat ctermbg=black guibg=black
+      colorscheme gruvbox
 
-        let g:deoplete#enable_at_startup = 1
-        call deoplete#custom#option('keyword_patterns', {'clojure': '[\w!$%&*+/:<=>?@\^_~\-\.#]*'})
-        set completeopt-=preview
+      let mapleader = "," 
+      let maplocalleader = ","
 
-        let g:float_preview#docked = 0
-        let g:float_preview#max_width = 80
-        let g:float_preview#max_height = 40
+      nnoremap <leader>n :NERDTreeFocus<CR>
+      nnoremap <C-n> :NERDTreeToggle<CR>
 
-        let g:ale_linters = { 'clojure': ['clj-kondo', 'joker'] }
+      let g:float_preview#docked = 0
+      let g:float_preview#max_width = 80
+      let g:float_preview#max_height = 40
+
+      highlight NormalFloat ctermbg=black guibg=black
+
+      let g:deoplete#enable_at_startup = 1
+      call deoplete#custom#option('keyword_patterns', {'clojure': '[\w!$%&*+/:<=>?@\^_~\-\.#]*'})
+      set completeopt-=preview
+
+      let g:ale_linters = { 'clojure': ['clj-kondo', 'joker'] }
     '';
 
+    extraLuaConfig = ''
+      vim.keymap.set('t', '<esc>', [[<C-\><C-n>]], opts)
+    '';
+    
     plugins = with pkgs.vimPlugins; [
       # General
+      vim-airline
+      {
+        plugin = toggleterm-nvim;
+        type = "lua";
+        config = ''
+          require("toggleterm").setup({
+            open_mapping = [[<leader>q]],
+            start_in_insert = true,
+            direction = "float",
+          })
+        '';
+      }
+      gruvbox
+      nerdtree
       fzf-vim
       deoplete-nvim
       float-preview-nvim
@@ -133,7 +159,8 @@ in
       ale
       vim-dispatch vim-jack-in conjure
       vim-repeat vim-surround vim-sexp vim-sexp-mappings-for-regular-people
-      gruvbox
+      # Go
+      vim-go
     ];
   };
 }
