689eeca70b336d0b43b259adb4acdcdcf3c9eb7f
James Williams <james.williams@griffin.com>
2025-07-01 15:24:49 +0100

    fix: remove unneeded lsp-format setup

--- hands/nixvim.nix
+++ hands/nixvim.nix
@@ -341,15 +341,6 @@
         capabilities = capabilities
       }
 
-      -- lsp-format
-      require("lsp-format").setup {}
-      vim.api.nvim_create_autocmd('LspAttach', {
-        callback = function(args)
-          local client = assert(vim.lsp.get_client_by_id(args.data.client_id))
-          require("lsp-format").on_attach(client, args.buf)
-        end,
-      })
-
       -- lsp-lines
       vim.diagnostic.config({virtual_lines = true})
 
